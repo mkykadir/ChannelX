9949dea77bb21a1627b238366dea3f3a72ce0bcf
ozkokmur <ozkokmur@itu.edu.tr>
2017-11-19 05:12:34 +0300

    Channels Table is Added

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from app import db
+from sqlalchemy.orm import relationship
 import hashlib
 import random
+import datetime
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -73,7 +75,28 @@ class Message(db.Model):
     def get_message(self):
         return self.messageItself
 
-   
+class Channel(db.Model):    
+    __tablename__ = 'channels'
+
+    name = db.Column(db.String(100), nullable = False, primary_key=True)
+    creator = db.Column(db.String(100), db.ForeignKey('users.username'), nullable = False)
+    description = db.Column(db.String(200),nullable = False)
+    start = db.Column(db.DateTime, nullable = True)
+    end = db.Column(db.DateTime, nullable = True)
+    creation_date = db.Column(db.DateTime, nullable = False)
+    member_limit = db.Column(db.Integer, nullable = True)
+    password = db.Column(db.Text, nullable = True)
+
+    def __init__(self, name, creator, description, start, end, member_limit):
+        self.name = name
+        self.creator = creator
+        self.description = description
+        self.start = start
+        self.end = end
+        self.member_limit = member_limit
+
+        self.creation_date = datetime.datetime.now()
+           
     
         
 
